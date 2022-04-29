067086b4177f82d59e440d8fa84e8c4a1577a2e9
JHONATANGARCIAXD <jhonatangarcia@colegioguanenta.edu.co>
2022-04-29 08:41:22 -0500

    Desarrollo: Ejercicio1

---
+++ ejercicio1/README.md
@@ -0,0 +1,5 @@
+Ejercicio 1
+
+Programa para hallar el cuadrante de coordenadas cartesionas
+
+[Diagrama de flujo](diagrama.png)
---
+++ ejercicio1/cordenadas_cartesianas.py
@@ -0,0 +1,38 @@
+
+
+import math
+
+# input
+
+x=int(input("Digite el valor de x:"))
+y=int(input("Digite el valor de y:"))
+
+
+# processing 
+
+if x>0 and y>0:
+    print("Las coordenadas están en el primer cuadradrante")
+elif(x<0 and y>0):
+    print("Las cordenadas están en el segundo cuadrante")
+elif (x<0 and y<0):
+    print("Las coordenadas estan en el tercer cuadrante ")
+elif (x>0 and y<0):
+    print("Las coordenadas estan en cuarto cuadrante")
+elif x==0 and y==0:
+   print("Las coordenadas estan en el eje X y Y")
+    
+
+    
+ 
+
+     
+
+
+
+
+
+
+
+    
+
+
