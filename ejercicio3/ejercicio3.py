337eb714608809be21c0d19030d41ab8e8c79ed2
JHONATANGARCIAXD <jhonatangarcia@colegioguanenta.edu.co>
2022-05-05 18:36:51 -0500

    Desarrollo:ejercicio3

---
+++ ejercicio3/ejercicio3.py
@@ -0,0 +1,40 @@
+" Ejercicio 3"
+
+
+# input
+
+
+
+
+
+
+
+Precio_costo=float(input("Digite el costo del producto:"))
+
+
+# processing
+
+
+if Precio_costo<=3000:
+    ganancia = 0,15 * Precio_costo
+
+else:
+    if Precio_costo<=(6000):
+        ganancia=500
+    else: Precio_costo>=(6000)
+    ganancia=Precio_costo*0,25
+    
+   
+
+Precio_Venta=Precio_costo + ganancia
+
+# ouput
+
+
+print("El Producto con costo de $ " + str( Precio_costo ) + " con ganancia de $ " + str(ganancia) + " queda con Precio de venta en $ " + str (Precio_Venta) )
+
+
+
+    
+
+
