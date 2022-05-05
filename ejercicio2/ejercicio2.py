fd0e986e0e9a79e522036749bdd631b08d9a8273
JHONATANGARCIAXD <jhonatangarcia@colegioguanenta.edu.co>
2022-05-05 17:20:56 -0500

    Desarrollo: ejercicio2

---
+++ ejercicio2/ejercicio2.py
@@ -0,0 +1,36 @@
+"ejercicio 2"
+
+
+
+
+
+# input
+
+from tokenize import Intnumber
+
+
+ingresos=int(input("Digite sus ingresos:"))
+
+
+# processing 
+
+if ingresos<945200:
+    print("No puede hacer el prestamo ya que sus ingresos son menores a $945200")
+
+
+if ingresos>=945200:
+    deudas=int(input("Tiene alguna deuda:SI TIENE Digite (1),SI NO TIENE Digite (2): "))
+    SN=deudas
+   
+if SN==(1):
+    print("No puede hacer el prestamo ya que tiene deudas puede hacer el presamo ya que no tiene deudas y sus ingresos son mayores a $945200")
+else:
+     SN==(2)
+     print("Puede hacer el presamo ya que no tiene deudas y sus ingresos son mayores a $945200")
+ 
+
+
+
+
+
+  
